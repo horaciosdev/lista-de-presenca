7b955824f5dfe0dfbeb15b4acc7a84e4ea1f7c98
Horacio <horacio.schumann@gmail.com>
2023-02-10 13:55:29 -0300

    feat: get header info from github api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card/Card";
@@ -6,6 +6,7 @@ import { Card } from "../../components/Card/Card";
 export const Home = () => {
   const [studentName, setStudentName] = useState("");
   const [students, setStudends] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: "" });
 
   function handleAddStudent() {
     const newStudent = {
@@ -20,13 +21,21 @@ export const Home = () => {
     setStudends((prevState) => [newStudent, ...prevState]);
   }
 
+  useEffect(() => {
+    fetch("https://api.github.com/users/horaciosdev")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({ name: data.name, avatar: data.avatar_url });
+      });
+  }, []);
+
   return (
     <div className="container">
       <header>
         <h1>Lista de Presença</h1>
         <div>
-          <strong>Horácio</strong>
-          <img src="https://github.com/horaciosdev.png" alt="" />
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="" />
         </div>
       </header>
       <input
