01c62353b32b8dfde9dcb064b9a0a832168792d8
Horacio <horacio.schumann@gmail.com>
2023-02-10 13:13:06 -0300

    fix: fix import of the Home component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
-import './styles/global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./pages/Home/Home";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,18 @@ import "./styles.css";
 import { Card } from "../../components/Card/Card";
 
 export const Home = () => {
+  const handleNameChange = (name) => {
+    console.log(name);
+  };
+
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => handleNameChange(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Horacio" time="10:23:23" />
