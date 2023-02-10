e1a7fd1924d96b689bfcf16aa69d410c50696d5d
Horacio <horacio.schumann@gmail.com>
2023-02-10 13:01:30 -0300

    feat: add Card and Card styles.

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export const Card = () => {
+  return (
+    <div className="card">
+      <strong>Horacio Schumann</strong>
+      <small>10:15:23</small>
+    </div>
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import "./styles.css";
 
+import { Card } from "../../components/Card";
+
 export const Home = () => {
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
